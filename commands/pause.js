ee01fbbb312f48c90e50c4952f71c3fca8f34b81
Brycen Addison <gl4cialize@gmail.com>
2021-10-12 02:16:27 -0400

    Added pause

--- commands/pause.js
+++ commands/pause.js
@@ -6,6 +6,28 @@ module.exports = {
         .setName('pause')
         .setDescription(commands.pause),
     async execute(interaction) {
-        await interaction.reply('Not yet implemented.');
+        const serverQueue = interaction.client.queue.get(interaction.guild.id);
+
+        if (!serverQueue) {
+            return await interaction
+                .followUp('nothins playin right now dawg')
+                .catch(console.error);
+        }
+
+        if (serverQueue.playing) {
+            serverQueue.audioPlayer.pause();
+            serverQueue.playing = false;
+            return await interaction
+                .followUp(
+                    'Playback has been paused. Use `/pause` again to resume playback.'
+                )
+                .catch(console.error);
+        } else {
+            serverQueue.audioPlayer.unpause();
+            serverQueue.playing = true;
+            return await interaction
+                .followUp('Playback has been resumed.')
+                .catch(console.error);
+        }
     }
 };
